59c1ef02691d9e4737646cbe4a923e16394c518f
ddc <ddc@users.noreply.github.com>
2024-01-16 15:40:51 -0300

    feat: version_1.0.4

--- tests/unit/test_file_utils.py
+++ tests/unit/test_file_utils.py
@@ -2,7 +2,7 @@
 import os
 from pathlib import Path
 import pytest
-from ddcUtils import constants, file_utils
+from ddcUtils import constants, FileUtils
 
 
 class TestFileUtils:
@@ -20,54 +20,54 @@ class TestFileUtils:
     def test_open_file(self):
         # nonexistent file
         with pytest.raises(FileNotFoundError) as exc_info:
-            file_utils.open_file(self.unknown_file)
+            FileUtils().open_file(self.unknown_file)
         assert exc_info.value.args[0] == 2
         assert exc_info.value.args[1] == "No such file or directory"
         assert exc_info.typename == "FileNotFoundError"
 
     def test_list_files(self):
         # list all files
-        result = file_utils.list_files(self.test_files_dir)
+        result = FileUtils().list_files(self.test_files_dir)
         assert Path(self.test_file) in result
 
         # list all files by exntension
-        result = file_utils.list_files(self.test_files_dir, ".zip")
+        result = FileUtils().list_files(self.test_files_dir, ".zip")
         assert Path(self.test_zip_file) in result
 
     def test_gzip_file(self):
         # test gzip file and delete afterwards
         file_name = os.path.basename(self.test_file)
         result_file_path = os.path.join(self.test_files_dir, f"{file_name}.gz")
-        result = file_utils.gzip_file(self.test_file)
+        result = FileUtils().gzip_file(self.test_file)
         assert result == Path(result_file_path)
         os.remove(result_file_path)
 
     def test_unzip_file(self):
         # test unzip file and delete afterwards
-        result = file_utils.unzip_file(self.test_zip_file)
+        result = FileUtils().unzip_file(self.test_zip_file)
         assert result is not None
         test_file = os.path.join(self.test_files_dir, result.filelist[0].filename)
-        files_list = file_utils.list_files(self.test_files_dir)
+        files_list = FileUtils().list_files(self.test_files_dir)
         assert Path(test_file) in files_list
         os.remove(test_file)
 
     def test_get_all_file_values(self):
         # wrong file path
         with pytest.raises(FileNotFoundError) as exc_info:
-            file_utils.get_all_file_values(self.unknown_file)
+            FileUtils().get_all_file_values(self.unknown_file)
         assert exc_info.value.args[0] == 2
         assert exc_info.value.args[1] == "No such file or directory"
         assert exc_info.typename == "FileNotFoundError"
 
         # returnign one single dict - mixed values
         mixed_values = True
-        result = file_utils.get_all_file_values(self.test_file, mixed_values)
+        result = FileUtils().get_all_file_values(self.test_file, mixed_values)
         assert "files" in result
         assert "database" in result
 
         # return organized dict values
         mixed_values = False
-        result = file_utils.get_all_file_values(self.test_file, mixed_values)
+        result = FileUtils().get_all_file_values(self.test_file, mixed_values)
         assert "main" in result
         assert "database_credentials" in result
 
@@ -75,14 +75,14 @@ class TestFileUtils:
         # wrong file
         section_name = "main"
         with pytest.raises(FileNotFoundError) as exc_info:
-            file_utils.get_all_file_section_values(self.unknown_file, section_name)
+            FileUtils().get_all_file_section_values(self.unknown_file, section_name)
         assert exc_info.value.args[0] == 2
         assert exc_info.value.args[1] == "No such file or directory"
         assert exc_info.typename == "FileNotFoundError"
 
         # get all section values
         section_name = "main"
-        result = file_utils.get_all_file_section_values(self.test_file, section_name)
+        result = FileUtils().get_all_file_section_values(self.test_file, section_name)
         assert "files" in result
         assert "path_logs" in result
 
@@ -90,13 +90,13 @@ class TestFileUtils:
         # existing config_name
         section_name = "Database Credentials"
         config_name = "port"
-        result = file_utils.get_file_value(self.test_file, section_name, config_name)
+        result = FileUtils().get_file_value(self.test_file, section_name, config_name)
         assert result == 5432
 
         # nonexistent config_name
         section_name = "main"
         config_name = "filess"
-        result = file_utils.get_file_value(self.test_file, section_name, config_name)
+        result = FileUtils().get_file_value(self.test_file, section_name, config_name)
         assert result is None
 
     def test_set_file_value(self):
@@ -104,11 +104,11 @@ class TestFileUtils:
         section_name = "main"
         config_name = "path_logs"
         new_value = "/tmp/test_dir"
-        result = file_utils.set_file_value(self.test_file, section_name, config_name, new_value)
+        result = FileUtils().set_file_value(self.test_file, section_name, config_name, new_value)
         assert result is True
-        result = file_utils.get_file_value(self.test_file, section_name, config_name)
+        result = FileUtils().get_file_value(self.test_file, section_name, config_name)
         assert result == new_value
 
     def test_get_binary_type(self):
-        result = file_utils.get_exe_binary_type(self.test_file)
+        result = FileUtils().get_exe_binary_type(self.test_file)
         assert result == "Not an EXE file"
--- tests/unit/test_misc_utils.py
+++ tests/unit/test_misc_utils.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 from datetime import datetime, timezone
-from ddcUtils import constants, misc_utils
+from ddcUtils import constants, MiscUtils, Object
 
 
 class TestMiscUtils:
@@ -16,20 +16,28 @@ class TestMiscUtils:
         # test nonexistent git file
         default_branch_name = "main"
         constants.BASE_DIR = "/tmp"
-        result = misc_utils.get_active_branch_name(default_branch_name)
+        result = MiscUtils().get_active_branch_name(default_branch_name)
         assert result == default_branch_name
 
     def test_get_current_date_time_str_long(self):
-        result = misc_utils.get_current_date_time_str_long()
+        result = MiscUtils().get_current_date_time_str_long()
         now_utc = datetime.now(timezone.utc)
         assert result == now_utc.strftime(constants.DATE_TIME_FORMATTER_STR)
 
     def test_convert_datetime_to_str_short(self):
         now_utc = datetime.now(timezone.utc)
-        result = misc_utils.convert_datetime_to_str_short(now_utc)
+        result = MiscUtils().convert_datetime_to_str_short(now_utc)
         assert result == now_utc.strftime(f"{constants.DATE_FORMATTER} {constants.TIME_FORMATTER}")
 
     def test_convert_str_to_datetime_short(self):
         date_str = "2024-01-01 00:00:00.000000"
-        result = misc_utils.convert_str_to_datetime_short(date_str)
+        result = MiscUtils().convert_str_to_datetime_short(date_str)
         assert result == datetime.strptime(date_str, f"{constants.DATE_FORMATTER} {constants.TIME_FORMATTER}")
+
+    def test_object(self):
+        new_object = Object()
+        new_object.test = "tttt"
+        t1 = new_object.to_json()
+        t2 = new_object.to_dict()
+        assert type(t1) is str
+        assert type(t2) is dict
